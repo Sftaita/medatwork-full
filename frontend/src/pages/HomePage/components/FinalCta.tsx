2900ce0bf3924b03e60bc11508ca8ce19d779eb0
Samy Ftaita <samy.ftaita89@gmail.com>
2026-05-27 11:54:18 +0200

    content: enlever le nom de famille dans le témoignage Brigitte

--- frontend/src/pages/HomePage/components/FinalCta.tsx
+++ frontend/src/pages/HomePage/components/FinalCta.tsx
@@ -139,7 +139,7 @@ const FinalCta = () => {
                   flexShrink: 0,
                 }}
               >
-                BD
+                B
               </Box>
               <Box>
                 <Typography sx={{ color: "#fff", fontWeight: 600, fontSize: 14.5, mb: "1px" }}>
